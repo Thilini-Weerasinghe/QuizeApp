d6b07daa7cf67b93c479f391ac06a0976eba0eab
Thilini-Weerasinghe <thiliniweerasinghe09@gmail.com>
2018-10-29 06:40:34 +0530

    create 5 questions for app

---
+++ app.js
@@ -0,0 +1,8 @@
+var questions =[
+
+new Question("Which one is not in object oriented programming language?",["java","c#","c++","c"],"c"),
+new Question("Which language is used for styling the web pages?",["HTML","JQuery","css","XML"] ,"css"),
+new Question("There are ___ main components of object oriented programming .",["1","6","3","4"],"4"),
+new Question("Which language is used for web applications?",["PHP","Phython","JavaScript","All"],"All"),
+new Question("MVC is a ___",["language","library","framework","All"],"framework"),
+];
